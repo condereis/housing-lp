2c2c40460c05ca18d335bd1b392efa396fe87498
Rafael Reis <rafael.lcreis@gmail.com>
2022-02-06 21:01:51 -0300

    Change basedir on settings

--- housing_lp/settings.py
+++ housing_lp/settings.py
@@ -58,9 +58,6 @@ ROOT_URLCONF = 'housing_lp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'templates'
-        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -82,7 +79,7 @@ WSGI_APPLICATION = 'housing_lp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
